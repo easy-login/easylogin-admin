3ec8611487d56e3b0e7ebba55c53b811e76c9a9a
tjeubaoit <anhtn.bk@gmail.com>
2018-10-14 01:40:01 +0700

    Fix cannot serving static file

--- SocialPlus/settings.py
+++ SocialPlus/settings.py
@@ -155,12 +155,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = BASE_DIR + '/loginapp/templates/loginapp/'
+STATIC_URL = '/static/'
 
-# STATUC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATIC_URL),
+    os.path.join(BASE_DIR, 'loginapp/templates/loginapp'),
 ]
 
 # App SETTING
