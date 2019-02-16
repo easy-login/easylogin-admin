24c1b5dd302578b2e47e3405ed8796a5c1d0dc6d
kumin <minhhoangvan@admicro.vn>
2019-02-16 23:24:39 +0700

    + fixed bug confirm deleting

--- loginapp/templates/loginapp/js/my.js
+++ loginapp/templates/loginapp/js/my.js
@@ -1,28 +1,3 @@
-function deleteConfirm(event){
-    event.preventDefault();
-    var form = event.target.closest('form');
-    swal({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                type: 'warning',
-                showCancelButton: true,
-                cancelButtonColor: '#4fa7f3',
-                confirmButtonColor: '#98a6ad',
-                reverseButtons: true,
-                confirmButtonText: 'Yes, delete it!',
-                onOpen: function(ele) {
-                    $('.swal2-cancel').focus()
-                }
-            }).then(function () {
-            form.submit();
-                swal(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
-                );
-            });
-}
-
 function activeMenu(){
     var url = window.location.pathname;
     var urlSplit = url.split('/')
