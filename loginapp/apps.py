9908c62f2ab844ec86548a5aeb91e2fe7fbf3638
tjeubaoit <anhtn.bk@gmail.com>
2020-04-22 02:24:29 +0700

    Add log

--- loginapp/apps.py
+++ loginapp/apps.py
@@ -8,5 +8,6 @@ class SocialPlusConfig(AppConfig):
     name = 'loginapp'
 
     def ready(self):
+        print('App ready, run init_providers script')
         with connection.cursor() as cursor:
             init_providers(connection, cursor)
