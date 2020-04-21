50ee5e11ff801e6abb23a306f3a0ef900fd315e2
tjeubaoit <anhtn.bk@gmail.com>
2020-04-22 03:03:41 +0700

    Avoid error at startup time

--- loginapp/apps.py
+++ loginapp/apps.py
@@ -11,20 +11,23 @@ class SocialPlusConfig(AppConfig):
     name = 'loginapp'
 
     def ready(self):
-        print('App ready, run init_providers script')
-        with connection.cursor() as cursor:
-            init_providers(connection, cursor)
+        try:
+            print('App ready, run init_providers script')
+            with connection.cursor() as cursor:
+                init_providers(connection, cursor)
 
-        print('Creating admin account...')
-        hasher = PBKDF2PasswordHasher()
-        hashed_pw = hasher.encode('Mirabo!23', uuid.uuid4().hex[:16])
+            print('Creating admin account...')
+            hasher = PBKDF2PasswordHasher()
+            hashed_pw = hasher.encode('Mirabo!23', uuid.uuid4().hex[:16])
 
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT id FROM admins WHERE username = 'admin'")
-            row = cursor.fetchone()
-            if not row:
-                cursor.execute("""
-                    INSERT INTO admins (username, email, password, is_superuser, level, first_name, last_name)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s)
-                """, ('admin', 'anhtn@mirabo.com.vn', hashed_pw, 1, 65535, 'Mirabo', 'Admin'))
-                connection.commit()
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT id FROM admins WHERE username = 'admin'")
+                row = cursor.fetchone()
+                if not row:
+                    cursor.execute("""
+                        INSERT INTO admins (username, email, password, is_superuser, level, first_name, last_name)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    """, ('admin', 'anhtn@mirabo.com.vn', hashed_pw, 1, 65535, 'Mirabo', 'Admin'))
+                    connection.commit()
+        except Exception as e:
+            print(e)
